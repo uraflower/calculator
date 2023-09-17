7fc6dde6e5ed43d8fa2876eabb782a14698434c0
uraflower <82873315+uraflower@users.noreply.github.com>
2023-09-17 15:33:14 +0900

    ADD | 연산자 입력 표시

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const input = document.querySelector('.input');
 const output = document.querySelector('.output');
 const numberKeyList = document.querySelectorAll('.number');
+const operatorKeyList = document.querySelectorAll('.operator');
 
 // 숫자 입력 핸들러
 const setInputNumber = (number) => {
@@ -8,9 +9,20 @@ const setInputNumber = (number) => {
   else input.innerText = input.innerText + number;
 };
 
+// 연산자 입력 핸들러
+const setInputOperator = (operator) => {
+  if (isNaN(input.innerText[input.innerText.length - 1]))
+    input.innerText = input.innerText.slice(0, -1) + operator;
+  else input.innerText = input.innerText + operator;
+};
+
 // onclick 이벤트 핸들러
 const onClickNumber = (event) => {
   setInputNumber(event.target.innerText);
+};
+
+const onClickOperator = (event) => {
+  setInputOperator(event.target.innerText);
 };
 
 // key 각각에 클릭 이벤트 리스너 달기
@@ -18,3 +30,6 @@ numberKeyList.forEach((key) => {
   key.addEventListener('click', onClickNumber);
 });
 
+operatorKeyList.forEach((key) => {
+  key.addEventListener('click', onClickOperator);
+});
